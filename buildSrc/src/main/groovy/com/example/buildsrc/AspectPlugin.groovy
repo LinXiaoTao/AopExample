70dd70203a44efe5f8e3532b6a55a86e14060294
leo <linxiaotao1993@gmail.com>
2017-03-06 21:39:52 +0800

    修改代码

--- buildSrc/src/main/groovy/com/example/buildsrc/AspectPlugin.groovy
+++ buildSrc/src/main/groovy/com/example/buildsrc/AspectPlugin.groovy
@@ -1,8 +1,10 @@
 package com.example.buildsrc
 
+import com.sun.tools.attach.VirtualMachine
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import java.lang.management.ManagementFactory
 
 public class AspectPlugin implements Plugin<Project>{
 
@@ -11,6 +13,12 @@ public class AspectPlugin implements Plugin<Project>{
     void apply(Project project) {
         println '==================================================================================='
         println "自定义插件"
+        def currentName = ManagementFactory.getRuntimeMXBean().getName()
+        def pid = currentName.split("@")[0]
+        println "当前进程pid: " + pid
+        VirtualMachine virtualMachine = VirtualMachine.attach(pid)
+        virtualMachine.loadAgent("/Users/linxiaotao/Documents/work/Personal/AopExample/buildSrc/agent.jar")
+        virtualMachine.detach()
         println '==================================================================================='
     }
 }
